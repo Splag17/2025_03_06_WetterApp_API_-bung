824bc286e14dcd34e0ce270e6f559712a5a3e94b
Splag17 <julian.eichhorn17@gmail.com>
2025-03-09 16:55:46 +0100

    Erste Temperaturausgabe

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Test")
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+City = input("Gib eine Stadt ein: ")
+
+URL = f"https://api.weatherapi.com/v1/current.json?key={API_KEY}&q={City}&lang=de"
+
+response = requests.get(URL)
+
+if response.status_code == 200:
+    data = response.json()
+    cityname = data["location"]["name"]
+    region = data["location"]["region"]
+    country = data["location"]["country"]
+    temperature = data["current"]["temp_c"]
+    print(f"Die Temperatur in {City} liegt bei {temperature}°C")
+    print(f"Stadt = {cityname} in {region}, {country}")
+else:
+    print("Fehler! Überprüfe den API-Schlüssel oder den Stadtnamen.")
